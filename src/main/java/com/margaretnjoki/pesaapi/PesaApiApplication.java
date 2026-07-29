7b1daef7ed774c6f602799440bcb9a9a8c627fdd
margaretnjoki <maggykiroko@gmail.com>
2026-07-29 05:59:55 +0300

    initial

---
+++ src/main/java/com/margaretnjoki/pesaapi/PesaApiApplication.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.pesaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margaretnjoki/pesaapi/PesaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.pesaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
